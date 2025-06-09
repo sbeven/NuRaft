1a6f6da49e569d26655d35534acd52a02cae5e4d
szhang99 <szhang99@terpmail.umd.edu>
2025-06-09 19:26:34 -0400

    goal:change log store to write to file-setup

--- examples/nuraft-logging/nuraft-logging.cxx
+++ examples/nuraft-logging/nuraft-logging.cxx
@@ -15,7 +15,7 @@ See the License for the specific language governing permissions and
 limitations under the License.
 **************************************************************************/
 
-#include "logging_state_machine.hxx"
+#include "nl_state_machine.hxx"
 #include "in_memory_state_mgr.hxx"
 #include "logger_wrapper.hxx"
 
